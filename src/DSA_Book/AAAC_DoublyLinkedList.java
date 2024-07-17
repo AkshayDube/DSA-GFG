dc2c64cef79558e777697ec45d60f5a4b1abe249
AkshayDube <dubeyakshay09@gmail.com>
2024-07-18 01:22:37 +0530

    Added insertAtGivenPositionFunctionality.

--- src/DSA_Book/AAAC_DoublyLinkedList.java
+++ src/DSA_Book/AAAC_DoublyLinkedList.java
@@ -9,6 +9,13 @@ public class AAAC_DoublyLinkedList {
         head = insertAtLast(head, 15);
         System.out.println("Total Elements : " + countElements(head));
         printList(head);
+        head = insertAtPosition(head,25,-1);
+        head = insertAtPosition(head,25,6);
+        head = insertAtPosition(head,100,1);//insert at beginning
+        head = insertAtPosition(head,110,6);//insert at last
+        head = insertAtPosition(head,120,4);
+        System.out.println("Total Elements : " + countElements(head));
+        printList(head);
     }
 
     public static int countElements(DNode head) {
@@ -37,6 +44,7 @@ public class AAAC_DoublyLinkedList {
         return newDNode;
     }
 
+    //insertAtLast
     public static DNode insertAtLast(DNode head, int data) {
         DNode newNode = new DNode(data);
         if (null == head) {
@@ -52,6 +60,42 @@ public class AAAC_DoublyLinkedList {
         return head;
     }
 
+    //insertAtSpecifiedPosition
+    public static DNode insertAtPosition(DNode head, int data, int position) {
+        if (position < 1) {
+            System.out.println("Negative Position not allowed");
+            return head;
+        }
+        DNode newNode = new DNode(data);
+        //insertionAtBeginning
+        if (position == 1) {
+            newNode.next = head;
+            head.prev = newNode;
+            return newNode;
+        }
+        int counter = 1;
+        DNode temp = head;
+        while (counter < position - 1 && temp != null) { //1 2 3 4 5
+            counter++;
+            temp = temp.next;
+        }
+        if (temp == null) {
+            System.out.println("Position out of range");
+            return head;
+        }
+        if (temp.next == null) {//insertion At last
+            newNode.next = temp.next;
+            newNode.prev = temp;
+            temp.next = newNode;
+            return head;
+        }
+        newNode.next = temp.next;
+        newNode.prev = temp;
+        temp.next.prev = newNode;
+        temp.next = newNode;
+        return head;
+
+    }
 }
 
 class DNode {
